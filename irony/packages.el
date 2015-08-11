281b0d01feae44d74b9294b7f18aaaee919170e3
jain.y <jain_y@126.com>
2015-08-11 19:26:08 +0800

    Add company-irony & company-irony-c-headers

--- irony/packages.el
+++ irony/packages.el
@@ -16,6 +16,8 @@
       '(
         ;; package names go here
         irony
+        company-irony
+        company-irony-c-headers
         ))
 
 ;; List of packages to exclude.
@@ -32,25 +34,47 @@
 ;; https://github.com/jwiegley/use-package
 
 (defun irony/init-irony ()
-      (use-package irony
-        :defer t
-        :init
-        (progn
-          (add-hook 'c++-mode-hook 'irony-mode)
-          (add-hook 'c-mode-hook 'irony-mode)
-          (add-hook 'objc-mode-hook 'irony-mode)
+  (use-package irony
+    :defer t
+    :init
+    (progn
+      (add-hook 'c++-mode-hook 'irony-mode)
+      (add-hook 'c-mode-hook 'irony-mode)
+      (add-hook 'objc-mode-hook 'irony-mode)
 
-          ;; replace the `completion-at-point' and `complete-symbol' bindings in
-          ;; irony-mode's buffers by irony-mode's function
-          (defun my-irony-mode-hook ()
-            (define-key irony-mode-map [remap completion-at-point]
-              'irony-completion-at-point-async)
-            (define-key irony-mode-map [remap complete-symbol]
-              'irony-completion-at-point-async))
-          (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-          ;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+      ;; replace the `completion-at-point' and `complete-symbol' bindings in
+      ;; irony-mode's buffers by irony-mode's function
+      (defun my-irony-mode-hook ()
+        (define-key irony-mode-map [remap completion-at-point]
+          'irony-completion-at-point-async)
+        (define-key irony-mode-map [remap complete-symbol]
+          'irony-completion-at-point-async))
+      (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+      ;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
 
-          (setq w32-pipe-read-delay 0)
-          )
-        )
+      (setq w32-pipe-read-delay 0)
       )
+    )
+  )
+
+(defun irony/init-company-irony ()
+  (use-package company-irony
+    :defer t
+    :init
+    (progn
+      (eval-after-load 'company
+        '(add-to-list 'company-backends 'company-irony)))
+    )
+  )
+
+(defun irony/init-company-irony-c-headers ()
+  (use-package company-irony-c-headers
+    :defer t
+    :init
+    (progn
+      ;; Load with `irony-mode` as a grouped backend
+      (eval-after-load 'company
+        '(add-to-list
+          'company-backends '(company-irony-c-headers company-irony))))
+   )
+  )
