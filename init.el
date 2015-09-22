92550205435cc05bcb34e2e524a7a96c1ff36fb3
jain.y <jain_y@126.com>
2015-09-22 15:50:30 +0800

    Add dokuwiki-mode

--- init.el
+++ init.el
@@ -50,6 +50,7 @@
      vbnet
      myorg
      jain
+     dokuwiki-mode
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
