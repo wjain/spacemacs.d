f46c7b58d6a6916218770b7a8f5cd2769d2fd11f
jain.y <jain_y@126.com>
2015-09-24 10:01:30 +0800

    Add LibreOffice path on mac

--- myorg/packages.el
+++ myorg/packages.el
@@ -54,70 +54,70 @@
             ))
 
     (add-to-list 'org-latex-classes  '("ctexart" "\\documentclass[10pt,a4paper]{article}
-                \\usepackage{graphicx}
-                \\usepackage{xcolor}
-                \\usepackage{xeCJK}
-                \\usepackage{lmodern}
-                \\usepackage{verbatim}
-                \\usepackage{fixltx2e}
-                \\usepackage{longtable}
-                \\usepackage{float}
-                \\usepackage{tikz}
-                \\usepackage{wrapfig}
-                \\usepackage{soul}
-                \\usepackage{textcomp}
-                \\usepackage{minted}
-                \\usepackage{listings}
-                \\usepackage{geometry}
-                \\usepackage{algorithm}
-                \\usepackage{algorithmic}
-                \\usepackage{marvosym}
-                \\usepackage{wasysym}
-                \\usepackage{latexsym}
-                \\usepackage{natbib}
-                \\usepackage{fancyhdr}
-                \\usepackage[xetex,colorlinks=true,CJKbookmarks=true,
-                linkcolor=blue,
-                urlcolor=blue,
-                menucolor=blue]{hyperref}
-                \\usepackage{fontspec,xunicode,xltxtra}
-                \\setmainfont[BoldFont=SimHei]{SimSun}
-                \\setsansfont[BoldFont=SimHei]{SimKai}
-                \\setmonofont{Courier}
-                \\newcommand\\fontnamemono{SimKai}%等宽字体
-                \\newfontinstance\\MONO{\\fontnamemono}
-                \\newcommand{\\mono}[1]{{\\MONO #1}}
-                \\setCJKmainfont[Scale=0.9]{SimHei}%中文字体
-                \\setCJKmonofont[Scale=0.9]{SimHei}
-                \\hypersetup{unicode=true}
-                \\geometry{a4paper, textwidth=6.5in, textheight=10in,
-                marginparsep=7pt, marginparwidth=.6in}
-                \\definecolor{foreground}{RGB}{220,220,204}%浅灰
-                \\definecolor{background}{RGB}{62,62,62}%浅黑
-                \\definecolor{preprocess}{RGB}{250,187,249}%浅紫
-                \\definecolor{var}{RGB}{239,224,174}%浅肉色
-                \\definecolor{string}{RGB}{154,150,230}%浅紫色
-                \\definecolor{type}{RGB}{225,225,116}%浅黄
-                \\definecolor{function}{RGB}{140,206,211}%浅天蓝
-                \\definecolor{keyword}{RGB}{239,224,174}%浅肉色
-                \\definecolor{comment}{RGB}{180,98,4}%深褐色
-                \\definecolor{doc}{RGB}{175,215,175}%浅铅绿
-                \\definecolor{comdil}{RGB}{111,128,111}%深灰
-                \\definecolor{constant}{RGB}{220,162,170}%粉红
-                \\definecolor{buildin}{RGB}{127,159,127}%深铅绿
-                \\punctstyle{kaiming}
-                \\title{}
-                \\fancyfoot[C]{\\bfseries\\thepage}
-                \\chead{\\MakeUppercase\\sectionmark}
-                \\pagestyle{fancy}
-                \\tolerance=1000
-                [NO-DEFAULT-PACKAGES]
-                [NO-PACKAGES]"
-                   ("\\section{%s}" . "\\section*{%s}")
-                   ("\\subsection{%s}" . "\\subsection*{%s}")
-                   ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                   ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                   ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+                                        \\usepackage{graphicx}
+                                        \\usepackage{xcolor}
+                                        \\usepackage{xeCJK}
+                                        \\usepackage{lmodern}
+                                        \\usepackage{verbatim}
+                                        \\usepackage{fixltx2e}
+                                        \\usepackage{longtable}
+                                        \\usepackage{float}
+                                        \\usepackage{tikz}
+                                        \\usepackage{wrapfig}
+                                        \\usepackage{soul}
+                                        \\usepackage{textcomp}
+                                        \\usepackage{minted}
+                                        \\usepackage{listings}
+                                        \\usepackage{geometry}
+                                        \\usepackage{algorithm}
+                                        \\usepackage{algorithmic}
+                                        \\usepackage{marvosym}
+                                        \\usepackage{wasysym}
+                                        \\usepackage{latexsym}
+                                        \\usepackage{natbib}
+                                        \\usepackage{fancyhdr}
+                                        \\usepackage[xetex,colorlinks=true,CJKbookmarks=true,
+                                        linkcolor=blue,
+                                        urlcolor=blue,
+                                        menucolor=blue]{hyperref}
+                                        \\usepackage{fontspec,xunicode,xltxtra}
+                                        \\setmainfont[BoldFont=SimHei]{SimSun}
+                                        \\setsansfont[BoldFont=SimHei]{SimKai}
+                                        \\setmonofont{Courier}
+                                        \\newcommand\\fontnamemono{SimKai}%等宽字体
+                                        \\newfontinstance\\MONO{\\fontnamemono}
+                                        \\newcommand{\\mono}[1]{{\\MONO #1}}
+                                        \\setCJKmainfont[Scale=0.9]{SimHei}%中文字体
+                                        \\setCJKmonofont[Scale=0.9]{SimHei}
+                                        \\hypersetup{unicode=true}
+                                        \\geometry{a4paper, textwidth=6.5in, textheight=10in,
+                                        marginparsep=7pt, marginparwidth=.6in}
+                                        \\definecolor{foreground}{RGB}{220,220,204}%浅灰
+                                        \\definecolor{background}{RGB}{62,62,62}%浅黑
+                                        \\definecolor{preprocess}{RGB}{250,187,249}%浅紫
+                                        \\definecolor{var}{RGB}{239,224,174}%浅肉色
+                                        \\definecolor{string}{RGB}{154,150,230}%浅紫色
+                                        \\definecolor{type}{RGB}{225,225,116}%浅黄
+                                        \\definecolor{function}{RGB}{140,206,211}%浅天蓝
+                                        \\definecolor{keyword}{RGB}{239,224,174}%浅肉色
+                                        \\definecolor{comment}{RGB}{180,98,4}%深褐色
+                                        \\definecolor{doc}{RGB}{175,215,175}%浅铅绿
+                                        \\definecolor{comdil}{RGB}{111,128,111}%深灰
+                                        \\definecolor{constant}{RGB}{220,162,170}%粉红
+                                        \\definecolor{buildin}{RGB}{127,159,127}%深铅绿
+                                        \\punctstyle{kaiming}
+                                        \\title{}
+                                        \\fancyfoot[C]{\\bfseries\\thepage}
+                                        \\chead{\\MakeUppercase\\sectionmark}
+                                        \\pagestyle{fancy}
+                                        \\tolerance=1000
+                                        [NO-DEFAULT-PACKAGES]
+                                        [NO-PACKAGES]"
+                                      ("\\section{%s}" . "\\section*{%s}")
+                                      ("\\subsection{%s}" . "\\subsection*{%s}")
+                                      ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                                      ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                                      ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
 
     (add-to-list 'org-latex-classes '("macart" "\\documentclass[11pt]{ctexart}
                                         [NO-DEFAULT-PACKAGES]
@@ -275,7 +275,11 @@
 (defun myorg/init-myorg-export-odt ()
   "Org export odt"
   (progn
-    (setq org-odt-convert-processes '(("LibreOffice" "/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to %f%x --outdir %d %i")))
+
+    (if (spacemacs/system-is-mac)
+        (setq org-odt-convert-processes '(("LibreOffice" "/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to %f%x --outdir %d %i")))
+      (setq org-odt-convert-processes '(("LibreOffice" "soffice --headless --convert-to %f%x --outdir %d %i"))))
+    
 
     ;; @see https://gist.github.com/mwfogleman/95cc60c87a9323876c6c
     (defun narrow-or-widen-dwim ()
