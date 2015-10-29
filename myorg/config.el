c4ceed207383671e6c761095a119b9fce83b7fb6
jain.y <jain_y@126.com>
2015-10-29 10:27:40 +0100

    set alert-default-style growl set alert-growl-command for windows.

---
+++ myorg/config.el
@@ -0,0 +1,21 @@
+;;; config.el --- Org configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+
+(setq alert-default-style 'growl)
+
+(if (spacemacs/system-is-mswindows)
+    (setq alert-growl-command "~/.spacemacs.d/plugins/growlforwindows/growlnotify.exe")
+ )
+
