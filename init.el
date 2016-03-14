c52cbcab43c90fa84afed300c07ed896a88cb30e
Jain.Y <jain_y@126.com>
2016-03-14 06:46:42 +0100

    set default theme solarized-dark

--- init.el
+++ init.el
@@ -129,10 +129,10 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light
-                         solarized-dark
+   dotspacemacs-themes '(solarized-dark
                          solarized-light
+                         spacemacs-dark
+                         spacemacs-light
                          monokai
                          zenburn)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
