9577283342f1b4a25d345e9b5ccdf05689d8675a
jain_y <jain_y@126.com>
2016-06-18 13:47:38 +0800

    Add code bug for php debug.

--- jain/packages.el
+++ jain/packages.el
@@ -16,11 +16,12 @@
       '(
         ;; package names go here
         ;; company-c-headers
-        my-web-mode
+        web-mode
         impatient-mode
         nodejs-repl
-        my-php-mode
+        php-mode
         geben
+        codebug
         (doxymacs :location local)
         ))
 
@@ -65,7 +66,7 @@
              (> (float-time) (float-time (date-to-time exp)))
            (error nil)))))
 
-(defun jain/init-my-web-mode ()
+(defun jain/post-init-web-mode ()
   "Initialize my package"
   (add-hook 'js2-mode-hook #'js2-refactor-mode)
   (add-hook 'js-mode-hook (lambda ()
@@ -90,7 +91,7 @@
     )
   )
 
-(defun jain/init-my-php-mode ()
+(defun jain/post-init-php-mode ()
   (use-package php-mode
     :init
     (when (spacemacs/system-is-mswindows)
@@ -102,6 +103,12 @@
 
 (defun jain/init-geben ()
   (use-package geben
+    :defer t
+    )
+  )
+
+(defun jain/init-codebug ()
+  (use-package codebug
     :defer t
     )
   )
