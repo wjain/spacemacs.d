ea26a434a02032f95182d96c2ed03259739650b6
jain.y <jain_y@126.com>
2021-04-08 08:48:06 +0800

    open eaf layer.

--- init.el
+++ init.el
@@ -107,6 +107,7 @@ This function should only modify configuration layer settings."
      pdf
      ;; common-lisp
      ;; restclient
+     eaf
      )
 
    ;; List of additional packages that will be installed without being
